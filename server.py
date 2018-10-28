b2b7221ee32e7f221152e5de7579f9101ff042e6
agidewall <44530167+agidewall@users.noreply.github.com>
2018-10-27 22:48:12 -0600

    Add files via upload
    Added server python, index page.

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+	    f = request.files['the_file']
+		f.save('/var/www/uploads/uploaded_file.txt')
