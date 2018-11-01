44b88789c6687217c72d6797bb6b44306eccf7c8
agidewall <44530167+agidewall@users.noreply.github.com>
2018-10-31 23:18:03 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,8 @@
 import sys
 
 from flask import Flask, render_template, request, redirect, Response
-from geocode import translate, toURL
-import random, json, urllib, time
+from geocode import translate, toURL, toRows, toJSON, writeHTML
+import random, json, urllib, time, csv
 
 app = Flask(__name__)
 
@@ -15,21 +15,25 @@ def output():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    return render_template('upload_page.html', info=request)
-    #if request.method == 'POST':
-	#    f = request.files['file']
-	#    f.save('/uploads/uploaded_file.txt')
-	#    return render_template('upload_page.html', info='hahahaha')
+    #return render_template('upload_page.html', info=request)
+    if request.method == 'POST':
+	    f = request.files['file']
+	    csvfile = f.read()
+	    rows = toRows(csvfile)
+	    string = ""
+	    for row in rows:
+		    string += writeHTML(toJSON(row))
+	    f.close()
+	    return render_template('upload_page.html', info=string)
 
 @app.route('/geo_address', methods=['GET', 'POST'])		
 def geoApi():
     js = translate(toURL(request.form['main_add']), toURL(request.form['city_add']), toURL(request.form['state_add']))
     if js['status'] == 'OK':
-	    latitude = js['geometry']['location']['lat']
-	    longitude = js['geometry']['location']['lng']
-	    return render_template('address_page.html', lat=latitude, lng=longitude, info="")
+	    string = writeHTML(js)
+	    return render_template('address_page.html', info=string)
     else:
-        return render_template('address_page.html', lat="", lng="", info=js)
+        return render_template('address_page.html', info=js)
 	
 if __name__ == 'main':
     app.run()
